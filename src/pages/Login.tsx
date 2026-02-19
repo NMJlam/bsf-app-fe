d3c1a1d13634da74336cd0385f6efb55b07d9a4a
NMJlam <nathan.lam.rt@gmail.com>
2026-02-19 21:09:00 +1100

    feat(Login): login page component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Login() {
-  return <h1> This is a Login page </h1>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-center">Login to your Account</CardTitle>
+        </CardHeader>
+        <CardFooter className="justify-center">
+          <Button className="w-full"> Login with Google </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
